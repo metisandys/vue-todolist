4a5e40a3565809b534548e375adc6a770b96f0ad
eddie <mengxiangyu94@163.com>
2017-08-31 11:53:08 +0800

    todolist use vue

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <h1 v-text="title"></h1>
+    <input v-model="newItem" v-on:keyup.enter="addNew">
+    <ul>
+      <li v-for="item in items" v-text="item.things" v-bind:class="{finished: item.isFinished}" v-on:click="toggleFinished(item)"></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data : function () {
+    return {
+      title: 'Hello Eddie ！This is your todo list 。',
+      items: [],
+      newItem: ''
+    }
+  },
+  methods: {
+    toggleFinished: function(item){
+      item.isFinished = !item.isFinished;
+    },
+    addNew: function(){
+      this.items.push({
+        things: this.newItem,
+        isFinished: false
+      });
+      this.newItem = '';
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.finished{
+  color: red;
+  text-decoration: underline;
+  
+}
+#app li{
+  cursor: pointer;
+}
+</style>
