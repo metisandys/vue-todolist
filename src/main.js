fda4051001a1ec3ed87feacff745ef519c74e9a6
eddie <mengxiangyu94@163.com>
2017-09-04 13:33:14 +0800

    first updata

--- src/main.js
+++ src/main.js
@@ -1,13 +1,53 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import 'todomvc-app-css/index.css';
 
-Vue.config.productionTip = false
+import Vue from 'vue';
 
-/* eslint-disable no-new */
+var filters = {
+	all(todos) {
+		return todos
+	},
+	active(todos) {
+		return todos.filter((todo) => {
+			return !todo.completed
+		})
+	},
+	completed(todos) {
+		return todos.filter((todo) => {
+			return todo.completed
+		})
+	}
+}
+
+
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+	el: '.todoapp',
+	data: {
+		title: 'todo-list',
+		newTodo: '',
+		todos: [{
+			content: 'vue',
+			completed: false
+		}, {
+			content: 'vuex',
+			completed: false
+		}]
+	},
+	computed: {
+		remain: function() {
+			console.log(filters.active(this.todos).length);
+			return filters.active(this.todos).length;
+		}
+	},
+	methods: {
+		addTodo(e) {
+			this.todos.push({
+				content: this.newTodo,
+				completed: false
+			});
+			this.newTodo = '';
+		},
+		removeTodo(index) {
+			this.todos.splice(index, 1);
+		}
+	}
 })
